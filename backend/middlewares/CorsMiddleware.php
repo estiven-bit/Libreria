168e46ac2c59449541bf83428efd11da2b21048e
Estiven <davila_v_23@hotmail.com>
2026-06-05 02:53:33 +0200

    debug: add debug headers to CorsMiddleware

--- backend/middlewares/CorsMiddleware.php
+++ backend/middlewares/CorsMiddleware.php
@@ -13,6 +13,10 @@ class CorsMiddleware
         $isAllowed = in_array($origin, $allowed, true)
             || (str_ends_with($origin, '.vercel.app') && $origin !== '');
 
+        header('X-Debug-Origin: ' . $origin);
+        header('X-Debug-Allowed: ' . json_encode($allowed));
+        header('X-Debug-IsAllowed: ' . ($isAllowed ? 'yes' : 'no'));
+
         if ($isAllowed) {
             header('Access-Control-Allow-Origin: ' . $origin);
         } elseif (!empty($allowed)) {
