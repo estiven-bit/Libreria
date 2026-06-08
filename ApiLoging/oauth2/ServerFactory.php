1ecbabad0bb2280411e151fecc72eff9d4499ebe
Estiven <davila_v_23@hotmail.com>
2026-06-08 22:24:19 +0200

    fix: sanitize OIDC issuer URL by trimming trailing slash to avoid double slashes

--- ApiLoging/oauth2/ServerFactory.php
+++ ApiLoging/oauth2/ServerFactory.php
@@ -28,7 +28,7 @@ class ServerFactory
 
     public static function issuer(): string
     {
-        return env('OIDC_ISSUER') ?: 'http://localhost:8000';
+        return rtrim(env('OIDC_ISSUER') ?: 'http://localhost:8000', '/');
     }
 
     public static function privateKeyPath(): string
